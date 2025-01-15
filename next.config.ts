afc0357dd50585d9f6109a3f56aad1c35a229892
Olise Onwuatogwu <oliseonwu@gmail.com>
2025-01-15 14:33:00 -0800

    Update next.config.ts
    changed spelling error

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/pomodoro-timer" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/pomodoroo-time" : "",
   images: {
     unoptimized: true,
   },
